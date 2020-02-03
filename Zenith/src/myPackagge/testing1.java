036a899cb7e257c853a9d34f4ca6734d3a8c1e09
Umang Sharma <48925718+umangsv@users.noreply.github.com>
2020-02-04 01:34:46 +0530

    master 1 step ahead now

---
+++ Zenith/src/myPackagge/testing1.java
@@ -0,0 +1,12 @@
+package myPackagge;
+
+public class testing1 {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println(" now master is step ahead and i need to pull latest master to local");
+
+	}
+
+}
