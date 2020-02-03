e33eecb5526822f7ad8e974b7856c44312a8e988
umangsv <umangsv1987@gmail,com>
2020-02-04 01:40:29 +0530

    stashing changes to testing1.java

--- Zenith/src/myPackagge/testing1.java
+++ Zenith/src/myPackagge/testing1.java
@@ -7,6 +7,7 @@ public class testing1 {
 
 		System.out.println(" now master is step ahead and i need to pull latest master to local");
 
+		System.out.println("let's make changes in to this file now !!");
 	}
 
 }
